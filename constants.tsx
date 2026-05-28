871206774b1a809de4ac3662ff3da9b5bbb94d63
Ishan <68177286+ishan711997@users.noreply.github.com>
2026-05-28 20:58:02 +0530

    update #4

--- constants.tsx
+++ constants.tsx
@@ -27,7 +27,7 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     category: "2D / Motion Graphics",
     youtubeUrl: "https://www.youtube.com/embed/JLDvYpxJb5I",
     description: "Explaination of math for the kids.",
-    viewCount: "58K"
+    viewCount: "5.8K"
   },
   {
     id: "v4",
@@ -35,7 +35,7 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     category: "3D Animation",
     youtubeUrl: "https://youtu.be/RjjHpIQMkYw?si=F_Qg_7IxER4Lszfc",
     description: "Orpat Group's smart fan 3d product advertisement.",
-    viewCount: "142K"
+    viewCount: "2.3K"
   },
   {
     id: "v5",
@@ -43,7 +43,7 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     category: "3D Animation",
     youtubeUrl: "https://youtu.be/g-Xo3mAqWn8?si=Kwx1-PsuX-_NG6Yu",
     description: "Charmecy Milano liquid foundaition product's ad",
-    viewCount: "31K"
+    viewCount: "3.1K"
   },
   {
     id: "v6",
@@ -51,7 +51,7 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     category: "Video Editing",
     youtubeUrl: "https://youtube.com/embed/krlfEtrzUC0",
     description: "Removed green screen understood the script and accordingly edit the video.",
-    viewCount: "18.5K"
+    viewCount: "9.4K"
   },
   {
     id: "v7",
@@ -59,8 +59,57 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     category: "2D / Motion Graphics",
     youtubeUrl: "https://youtube.com/embed/1BO02JKlDs8",
     description: "Iran Israel war explained through motion graphics for client",
-    viewCount: "18.5K"
-  }
+    viewCount: "3.1K"
+  },
+  {
+    id: "v8",
+    title: "Mumbai to Ahemdabad bullet train",
+    category: "2D / Motion Graphics",
+    youtubeUrl: "https://youtube.com/embed/omWspEEhJrM",
+    description: "Explaination of bullet train from mumbai to ahemdabad through motion graphics for client",
+    viewCount: "3.1K"
+  },
+  {
+    id: "v9",
+    title: "Jhelum river",
+    category: "2D / Motion Graphics",
+    youtubeUrl: "https://youtube.com/embed/P_YA0cKLl3s",
+    description: "Explaination of Jhelum River through motion graphics for client",
+    viewCount: "3.8K"
+  },
+  {
+    id: "v10",
+    title: "Arawali",
+    category: "2D / Motion Graphics",
+    youtubeUrl: "https://youtube.com/embed/BMAFlTnBkIg",
+    description: "Explaination of Arawali hill through motion graphics for client",
+    viewCount: "3.2K"
+  },
+  {
+    id: "v11",
+    title: "Constitution of india",
+    category: "2D / Motion Graphics",
+    youtubeUrl: "https://youtube.com/embed/npP8DMNIaPk",
+    description: "Explaination of Constitution of India through motion graphics for client",
+    viewCount: "8.3K"
+  },
+  {
+    id: "v12",
+    title: "Bharat Ratna designer Nandlal boase",
+    category: "2D / Motion Graphics",
+    youtubeUrl: "https://youtube.com/embed/qM8lOEgSaIA",
+    description: "Explaination of Bharat Ratna designer nandlal bose through motion graphics for client",
+    viewCount: "1.6K"
+  },
+  {
+    id: "v13",
+    title: "Queen of millets",
+    category: "2D / Motion Graphics",
+    youtubeUrl: "https://youtube.com/embed/dTBwldFaLEY",
+    description: "Explaination of Queen of millets through motion graphics for client",
+    viewCount: "10.5K"
+  },
+
 
 ];
 
